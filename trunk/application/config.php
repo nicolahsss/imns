8224c69fd7afc2638f3c4588a66c3201705766ec
neto.joaobatista <neto.joaobatista@444f1fa0-48b2-d210-f061-25d2dd88f669>
2010-07-30 10:30:34 +0000

    Adicionado exibição de erro E_ALL e E_STRICT e habilitado display_errors.
    Adição necessária para quem não tem habilitado por padrão no php.ini

--- trunk/application/config.php
+++ trunk/application/config.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting( E_ALL | E_STRICT );
+ini_set( 'display_errors' , 'On' );
+
 $include_path = ini_get( 'include_path' ) . PATH_SEPARATOR . realpath( '../application' );
 
 ini_set( 'include_path' , implode( PATH_SEPARATOR , array_unique( explode( PATH_SEPARATOR , $include_path ) ) ) );
