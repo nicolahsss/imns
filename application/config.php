c89b7a624c7487f5c2556db668c9a0a2823ab33a
19gui94 <19gui94@gmail.com>
2010-07-31 20:19:40 +0000

    Alguns comentários foram adicionados, nenhuma mudança no código

--- application/config.php
+++ application/config.php
@@ -1,13 +1,20 @@
 <?php
+/*
+ * Queremos saber de tudo!
+ */
 error_reporting( E_ALL | E_STRICT );
 ini_set( 'display_errors' , 'On' );
 
+/*
+ * Adiciona a pasta application ao include_path
+ */
 $include_path = ini_get( 'include_path' ) . PATH_SEPARATOR . realpath( '../application' );
-
 ini_set( 'include_path' , implode( PATH_SEPARATOR , array_unique( explode( PATH_SEPARATOR , $include_path ) ) ) );
 
+/*
+ * Registra a função para carregar classes automaticamente
+ */
 function loader( $class ) {
 	require sprintf( '%s.php' , implode( DIRECTORY_SEPARATOR , explode( '\\' , $class ) ) );
 }
-
 spl_autoload_register( 'loader' , true );
